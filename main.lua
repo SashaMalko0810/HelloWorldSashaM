e729c6b070c1333ae5f9b5e7b86994475f9bb449
Sasha Malko <sasha.malko@ocsbstudent.ca>
2018-02-26 10:45:43 -0500

    Changed the greeting

--- main.lua
+++ main.lua
@@ -21,7 +21,7 @@ display.setDefault('background', 51/255, 255/255, 233/255)
 local textObject
 
 --create the text object at position (300, 500) with a size 50
-textObject = display.newText ("Hello Sasha!", 50, 100, nil, 50)
+textObject = display.newText ("Hello Everyone!", 50, 100, nil, 50)
 
 --anchor the textObject from the top left corner
 textObject.anchorX = 0
